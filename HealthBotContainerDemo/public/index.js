5b55a11b45a9b8e0b2cd887149dcf4ed670ed30d
SaritSass <45786550+SaritSass@users.noreply.github.com>
2020-09-07 10:38:10 +0300

    Add files via upload

---
+++ HealthBotContainerDemo/public/index.js
@@ -0,0 +1,128 @@
+function requestChatBot(loc) {
+    const params = new URLSearchParams(location.search);
+    const locale = params.has('locale') ? params.get('locale') : 'en_us';
+    const oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", initBotConversation);
+    var path = "/chatBot?locale=" + locale;
+
+    if (loc) {
+        path += "&lat=" + loc.lat + "&long=" + loc.long;
+    }
+    if (params.has('userId')) {
+        path += "&userId=" + params.get('userId');
+    }
+    if (params.has('userName')) {
+        path += "&userName=" + params.get('userName');
+    }
+    oReq.open("POST", path);
+    oReq.send();
+}
+
+function chatRequested() {
+    const params = new URLSearchParams(location.search);
+    if (params.has('shareLocation')) {
+        getUserLocation(requestChatBot);
+    }
+    else {
+        requestChatBot();
+    }
+}
+
+function getUserLocation(callback) {
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+            var latitude  = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            var location = {
+                lat: latitude,
+                long: longitude
+            };
+            callback(location);
+        },
+        function(error) {
+            // user declined to share location
+            console.log("location error:" + error.message);
+            callback();
+        });
+}
+
+function initBotConversation() {
+    if (this.status >= 400) {
+        alert(this.statusText);
+        return;
+    }
+    // extract the data from the JWT
+    const jsonWebToken = this.response;
+    const tokenPayload = JSON.parse(atob(jsonWebToken.split('.')[1]));
+    const user = {
+        id: tokenPayload.userId,
+        name: tokenPayload.userName,
+        locale: tokenPayload.locale
+    };
+    let domain = undefined;
+    if (tokenPayload.directLineURI) {
+        domain =  "https://" +  tokenPayload.directLineURI + "/v3/directline";
+    }
+    var botConnection = window.WebChat.createDirectLine({
+        token: tokenPayload.connectorToken,
+        domain: domain
+    });
+    const styleOptions = {
+        botAvatarImage: '/images/avatar.png',
+        // botAvatarInitials: '',
+        // userAvatarImage: '',
+        hideSendBox: true, /* set to true to hide the send box from the view */
+        botAvatarInitials: 'קורי',
+        userAvatarInitials: 'אני',
+        backgroundColor: '#F8F8F8'
+    };
+
+    const store = window.WebChat.createStore({}, function(store) { return function(next) { return function(action) {
+        if (action.type === 'DIRECT_LINE/CONNECT_FULFILLED') {
+            store.dispatch({
+                type: 'DIRECT_LINE/POST_ACTIVITY',
+                meta: {method: 'keyboard'},
+                payload: {
+                    activity: {
+                        type: "invoke",
+                        name: "TriggerScenario",
+                        locale: user.locale,
+                        value: {
+                            trigger: "covid19_triage_new",
+                            args: {}
+                        }
+                    }
+                }
+            });
+        }
+        else if (action.type === 'DIRECT_LINE/INCOMING_ACTIVITY') {
+            if (action.payload && action.payload.activity && action.payload.activity.type === "event" && action.payload.activity.name === "ShareLocationEvent") {
+                // share
+                getUserLocation(function (location) {
+                    store.dispatch({
+                        type: 'WEB_CHAT/SEND_POST_BACK',
+                        payload: { value: JSON.stringify(location) }
+                    });
+                });
+            }
+            setTimeout(function () {
+                document.querySelector('div.css-y1c0xs').scrollTop = document.querySelector('div.css-y1c0xs').scrollHeight
+            });
+        }
+        return next(action);
+    }}});
+    const webchatOptions = {
+        directLine: botConnection,
+        styleOptions: styleOptions,
+        store: store,
+        userID: user.id,
+        username: user.name,
+        locale: user.locale
+    };
+    startChat(user, webchatOptions);
+}
+
+function startChat(user, webchatOptions) {
+    const botContainer = document.getElementById('webchat');
+    window.WebChat.renderWebChat(webchatOptions, botContainer);
+}
